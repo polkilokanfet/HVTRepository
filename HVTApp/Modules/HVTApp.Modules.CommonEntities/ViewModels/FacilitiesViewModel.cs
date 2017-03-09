4679bf378b6bf901f4b0998bdf6a4270d58eda50
kosolapov <kosolapov.ag@gmail.com>
2017-03-09 22:13:17 +0500

    Revert "FacilitiesViewModelChanged"
    This reverts commit 59301aaf2efdb36660864ee9619a709576bd37fb.

--- HVTApp/Modules/HVTApp.Modules.CommonEntities/ViewModels/FacilitiesViewModel.cs
+++ HVTApp/Modules/HVTApp.Modules.CommonEntities/ViewModels/FacilitiesViewModel.cs
@@ -3,17 +3,14 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using HVTApp.Infrastructure.Interfaces;
 
 namespace HVTApp.Modules.CommonEntities.ViewModels
 {
     public class FacilitiesViewModel : BindableBase
     {
-        private readonly IUnitOfWork _unitOfWork;
-
-        public FacilitiesViewModel(IUnitOfWork unitOfWork)
+        public FacilitiesViewModel()
         {
-            _unitOfWork = unitOfWork;
+
         }
     }
 }
